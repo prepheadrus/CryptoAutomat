373fea6004d0c6215994a2ee63c2f97356b0d4d4
Soligann <prepheadrus@gmail.com>
2025-12-21 09:11:33 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -51,7 +51,7 @@ const initialNodes: Node[] = [
 
 const initialEdges: Edge[] = [
   { id: 'e1-2', source: '1', target: '2', markerEnd: { type: MarkerType.ArrowClosed } },
-  { id:e2-3', source: '2', target: '3', markerEnd: { type: MarkerType.ArrowClosed } },
+  { id: 'e2-3', source: '2', target: '3', markerEnd: { type: MarkerType.ArrowClosed } },
 ];
 
 
