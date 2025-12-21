ec4da4087d445034910673d5d11d36c3bd85509a
Soligann <prepheadrus@gmail.com>
2025-12-21 13:24:13 +0000

    Try fixing this error: `Runtime ReferenceError: useCallback is not defin

--- src/app/bot-status/page.tsx
+++ src/app/bot-status/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, MouseEvent, useEffect, useMemo, useRef } from "react";
+import { useState, MouseEvent, useEffect, useMemo, useRef, useCallback } from "react";
 import Link from 'next/link';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
